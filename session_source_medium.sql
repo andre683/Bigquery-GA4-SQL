dbb3651e6ed5cec7cce929e494ba159b7d3ca74d
Andre Arias <aarias683@gmail.com>
2024-08-12 21:03:05 -0400

    Create session_source_medium.sql

---
+++ session_source_medium.sql
@@ -0,0 +1,28 @@
+WITH temp AS (
+  SELECT
+    CONCAT(user_pseudo_id, (SELECT value.int_value FROM UNNEST(event_params) WHERE key = 'ga_session_id')) AS session_id,
+    user_pseudo_id AS user_id,
+    COALESCE(
+      ARRAY_AGG((CASE WHEN collected_traffic_source.gclid IS NOT NULL THEN 'google' ELSE collected_traffic_source.manual_source END) 
+      IGNORE NULLS ORDER BY event_timestamp ASC LIMIT 1)[SAFE_OFFSET(0)], 
+      '(direct)') AS session_source,
+    COALESCE(
+      ARRAY_AGG((CASE WHEN collected_traffic_source.gclid IS NOT NULL THEN 'cpc' ELSE collected_traffic_source.manual_medium END) 
+      IGNORE NULLS ORDER BY event_timestamp ASC LIMIT 1)[SAFE_OFFSET(0)], 
+      '(none)') AS session_medium,
+    SUM(ecommerce.purchase_revenue) AS revenue,
+    COUNTIF(event_name = 'purchase') AS transactions
+  FROM `tough-healer-395417.analytics_287163560.events_*`
+  WHERE _TABLE_SUFFIX BETWEEN '20240701'AND '20240731'
+  GROUP BY event_date, session_id, user_pseudo_id
+)
+SELECT
+  session_source,
+  session_medium,
+  COUNT(DISTINCT user_id) AS total_users,
+  COUNT(DISTINCT session_id) AS sessions,
+  SUM(transactions) AS transactions,
+  SUM(revenue) AS revenue
+FROM temp
+GROUP BY session_source, session_medium
+ORDER BY total_users DESC
